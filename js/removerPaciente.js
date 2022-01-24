ffa844098952ebf27f112494103030ff62037fdc
Davi Bernardo <sbernardo.davi@outlook.com>
2022-01-24 15:47:09 -0300

    Feat: remover  pacientes com double click

---
+++ js/removerPaciente.js
@@ -0,0 +1,18 @@
+var pacientes = document.querySelectorAll(".paciente")
+
+// pacientes.forEach(function(paciente){
+//     paciente.addEventListener("dblclick", function(){
+//         console.log("Duplo clikcado");
+//         this.remove();
+//     });
+// });
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event){
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+
+    }, 500)
+});
